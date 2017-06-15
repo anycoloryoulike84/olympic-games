07e64393b0ae165d6d94c82af1c2d6d3b0b1ca44
anycoloryoulike84 <anycoloryoulike84@users.noreply.github.com>
2017-06-15 12:33:50 -0700

    server, get route for json

--- server/app.js
+++ server/app.js
@@ -1,2 +1,18 @@
-test
+
+"use strict";
+
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + '/../client'));
+
+app.get("/sports", function(request,response){
+	response.json(["Cycling", "Weightlifting"])
+});
+
+app.listen(8000, function(){
+console.log("listenng on 8000");
+});
+
+
 
