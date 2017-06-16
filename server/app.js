dc949104055e09032f288c4a8c2a33e42168badf
anycoloryoulike84 <anycoloryoulike84@users.noreply.github.com>
2017-06-15 17:24:45 -0700

    mean running, with curl sending request to server, server replying with sports collection object

--- server/app.js
+++ server/app.js
@@ -9,11 +9,18 @@ mongoUtil.connect();
 app.use(express.static(__dirname + '/../client'));
 
 app.get("/sports", function(request,response){
-	response.json(["Cycling", "Weightlifting", "Beer Drinking"])
+	var sports = mongoUtil.sports();
+	sports.find().toArray((err,docs) => {
+		if (err) {
+			console.log("mongo failed");
+		} else {
+				console.log(JSON.stringify(docs));}
+	});
+	// response.json(["Cycling", "Weightlifting", "Beer Drinking"])
 });
 
 app.listen(8000, function(){
-console.log("Listening on 8000");
+console.log(" \n Server running, Listening on 8000 ");
 });
 
 
--- server/mongoUtil.js
+++ server/mongoUtil.js
@@ -9,12 +9,14 @@ module.exports = {
 	connect() {
 		client.connect('mongodb://localhost:27017/olympics-dev', (err, db) => {
 	 if (err) {  
-	 	console.log("Error connecting to mongod");
+	 	console.log("\n Error connecting to mongod \n");
 		process.exit(1);
 	}
 	_db = db;
-	console.log("Connected to Mongo");
-	
+	console.log("\n Connected to Mongo, Server running \n");
 	});	
+   },
+   sports(){
+   		return _db.collection("sports");
    }
 }
