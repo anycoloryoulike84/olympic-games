5ae2af9b632942b5ad27311352d5054565344d1f
anycoloryoulike84 <anycoloryoulike84@users.noreply.github.com>
2017-06-15 17:59:51 -0700

    Server working, MEAN working, able to use mongo-cli to add, edit, view, delete collection data; *Successfully routes the MongoDB data to the server, AND to client

--- server/app.js
+++ server/app.js
@@ -12,11 +12,20 @@ app.get("/sports", function(request,response){
 	var sports = mongoUtil.sports();
 	sports.find().toArray((err,docs) => {
 		if (err) {
-			console.log("mongo failed");
+			console.log(" Mongo failed on Server");
 		} else {
-				console.log(JSON.stringify(docs));}
+			// convert 'docs' object into array named var sportsNames
+			var sportNames = docs.map( (sport) => sport.name);
+			response.json(sportNames);
+			console.log(" MongoDB returned: \n \n " + JSON.stringify(sportNames));
+			// response.json(["Cycling", "Weightlifting", "Beer Drinking"]);
+
+			}
 	});
-	// response.json(["Cycling", "Weightlifting", "Beer Drinking"])
+	
+
+
+	
 });
 
 app.listen(8000, function(){
