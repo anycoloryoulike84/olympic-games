135e25bb3b8ecb69ac07862489c8ee169297c168
anycoloryoulike84 <anycoloryoulike84@users.noreply.github.com>
2017-06-18 15:25:14 -0700

    s

--- server/app.js
+++ server/app.js
@@ -80,7 +80,7 @@ app.post('/sports/:name/medals', jsonParser, (request, response) => {
 
 
 
-app.listen(process.env.PORT || 8000), function(){
+app.listen(process.env.PORT || 5000, function(){
 console.log(" \n Server running, Listening on 8000 ");
 });
 
