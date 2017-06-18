b1550e8aafef32cd455c43efd70a4564dbbcb388
anycoloryoulike84 <anycoloryoulike84@users.noreply.github.com>
2017-06-18 14:27:59 -0700

    :wq dsdsd
    i with '#' will be ignored, and an empty message aborts the commit.

--- server/mongoUtil.js
+++ server/mongoUtil.js
@@ -24,5 +24,4 @@ module.exports = {
 // Remember to restart Mongo whenever we make a change in Endpoints
 // MongoDB URI through mLab: 
 // mongodb://heroku_n59wcpbj:ijmn97enc7ttpkuvef3mhnm6q1@ds129422.mlab.com:29422/heroku_n59wcpbj
-
 // heroku config:set PROD_MONGODB=mongodb://dbuser:dbpass@host1:port1,host2:port2/dbname
